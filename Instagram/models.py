947fa183d705da9c1381fdc3a1fd4ed71a480c96
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-12-19 11:32:26 -0600

    Fixed a typo

--- Instagram/models.py
+++ Instagram/models.py
@@ -7,4 +7,4 @@ class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     # document = models.FileField(upload_to='add/')
     # uploaded_at = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to='static/images')
+    photo = models.ImageField(upload_to='static/images/')
