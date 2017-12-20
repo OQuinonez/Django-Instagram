13ddb07539d555fd43fb83dfe58a27762c54cda3
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-12-20 13:43:25 -0600

    Erased some lines i had commented out becasue i was not using them anymore

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# from PIL import Image
-
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     # document = models.FileField(upload_to='add/')
     # uploaded_at = models.DateTimeField(auto_now_add=True)
-    photo = models.ImageField(upload_to='static/images/')
+    photo = models.ImageField(upload_to='Instagram/static/Instagram/images')
