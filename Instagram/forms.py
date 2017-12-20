a41b7057d564eb2c93dd8d66e84793c1f03ce3d2
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-12-20 13:38:24 -0600

    Added and image field to the class DisplayPicForm

--- Instagram/forms.py
+++ Instagram/forms.py
@@ -8,3 +8,7 @@ class AddPicForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ('description', 'photo', )
+
+
+class DisplayPicForm(forms.Form):
+    image = forms.ImageField()
