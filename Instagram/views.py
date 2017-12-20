a214201d78670e1e5e6ed6a64e79a5f32d38fa43
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-12-20 13:36:05 -0600

    Imported models, and add display_pic which is a function that should help display a picture

--- Instagram/views.py
+++ Instagram/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from .forms import AddPicForm
+from .models import Document
 
 # Create your views here.
 
@@ -11,10 +12,17 @@ def hello(request):
     return HttpResponse("Hello")
 
 
+def display_pic(request):
+    picture = [
+        picture.photo.url.replace('Instagram/static', '')
+        for picture in models.Document.objects.all()
+    ]
+
+
 def add_pic(request):
     form = AddPicForm(request.POST, request.FILES)
     if form.is_valid():
         form.save()
-        return redirect(hello(request))
+        return redirect(display_pic(request))
     else:
         return render(request, 'Instagram/add.html', {'form': form})
