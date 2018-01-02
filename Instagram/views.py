0af445cafca13ff9c5c85124949fc124ec502025
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-01-02 12:02:54 -0600

    Put the functions i already had inside classes and changed their name to what they do, depending if the get or post something, and started working on the AddFilter class

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,23 +1,55 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.http import HttpResponse
+from django.views import View
 from django.core.files.storage import FileSystemStorage
 from .forms import AddPicForm
 from Instagram import models
 from PIL import Image
 
-def display_pic(request):
-    pictures = [
-        picture.photo.url.replace('Instagram/static', '')
-        for picture in models.Document.objects.all()
-    ]
-    return render(request, 'Instagram/feed.html', {'pictures': pictures})
+
+class PicView(View):
+    def get(self, request):
+        pictures = [{
+            'url': picture.photo.url.replace('Instagram/static', ''),
+            'id': picture.id
+        } for picture in models.Document.objects.all()]
+        return render(request, 'Instagram/feed.html', {'pictures': pictures})
 
 
-def add_pic(request):
-    form = AddPicForm(request.POST, request.FILES)
-    if form.is_valid():
-        form.save()
-        return redirect('Instagram:feed')
-    else:
+class AddPic(View):
+    def get(self, request):
+        form = AddPicForm(request.POST, request.FILES)
         return render(request, 'Instagram/add.html', {'form': form})
+
+    def post(self, request):
+        form = AddPicForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('Instagram:feed')
+        else:
+            return render(request, 'Instagram/add.html', {'form': form})
+
+
+class DeletePic(View):
+    def post(self, request, image_id):
+        models.Document.objects.get(id=image_id).delete()
+        return redirect('Instagram:feed')
+
+
+class AddFilter(View):
+    def get(self, request, image_id):
+        form = Filters  #Comes from forms.py erase this comment after you're don
+        path = 'Instagram/static/' + models.Document.objects.get(
+            id=image_id).image_url()
+        return render(request, 'Instagram/feed', {'form': form})
+
+    def post(self, request, image_id):
+        form = Filters(request.POST)
+        path = 'Instagram/static' + models.Document.objects.get(
+            id=image_id).image_url()
+        image = Image.open(path)
+        if form.is_valid():
+            f = form.filter()
+            image.convert('RGB').filter(f).save(path)
+            return redirect('Instagram:feed')
